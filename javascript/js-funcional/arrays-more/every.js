1fc2b59ea520bb9f560e2ab8cf2a92b228abf894
riassp <senpaisla374@gmail.com>
2024-02-26 20:52:21 -0300

    changes

--- javascript/js-funcional/arrays-more/every.js
+++ javascript/js-funcional/arrays-more/every.js
@@ -7,6 +7,10 @@ const pessoas = [
 const isAdult = (pessoa) => pessoa.idade >= 18;
 
 console.log(pessoas.every(isAdult));
+
+
+
+
 /*
 Aqui está passando a function para o array e o array vai conferir se está certo e se estiver certo vai dar true, então se 
 todos tiverem com mais ou terem 18 anos vai ser true, se não, vai ser falso. 
