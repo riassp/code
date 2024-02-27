c4e985b4435c8826bcef7f611667d6399f393e8d
riassp <senpaisla374@gmail.com>
2024-02-27 17:33:03 -0300

    pratica

---
+++ javascript/js-funcional/startswith/scriptP.js
@@ -0,0 +1,20 @@
+const idades = [
+    'Rhuan',
+    'Henrique',
+    'Henry',
+    'Daniel',
+    'Pablo',
+    'Ryan',
+    'Paulo',
+    'John',
+    'Douglas',
+    'Jonathan',
+    'fabricio',
+    'fabio',
+    'andre',
+    'atila'
+];
+
+const conferirIdades = idades.filter((idade) => idade.startsWith('a'))
+
+console.log(conferirIdades);
