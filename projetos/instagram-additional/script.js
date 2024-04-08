996bded9ff46315e219e3055e95bf6f0a16e3336
riassp <senpaisla374@gmail.com>
2024-04-08 17:11:21 -0300

    changes

--- projetos/instagram-additional/script.js
+++ projetos/instagram-additional/script.js
@@ -1,5 +1,5 @@
 const contain = document.querySelector('#contain')
-
+contain.style.display = 'none';
 function fall() {
     if(contain.style.display === 'none'){
         contain.style.display = 'block'
