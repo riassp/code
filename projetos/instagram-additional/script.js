9972672f6f287a5d2fdf0213b49ac53336939a81
riassp <senpaisla374@gmail.com>
2024-04-09 17:13:03 -0300

    changes

--- projetos/instagram-additional/script.js
+++ projetos/instagram-additional/script.js
@@ -6,4 +6,16 @@ function fall() {
     }else{
         contain.style.display = 'none'
     }
-}
+};
+
+document.body.style.backgroundColor = 'black';
+function tema() {
+    if(document.body.style.backgroundColor === 'black'){
+        document.body.style.backgroundColor = 'white';
+        contain.style.backgroundColor = 'white';
+        body.p.style.Color = 'black';
+    } else{
+        document.body.style.backgroundColor = 'black';
+        contain.style.backgroundColor = '#262626';
+    }
+};
