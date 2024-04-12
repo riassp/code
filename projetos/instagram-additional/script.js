6d24c2cf1a4cb1470dd3c875641ba687b2e1de9f
riassp <senpaisla374@gmail.com>
2024-04-12 16:57:55 -0300

    changes

--- projetos/instagram-additional/script.js
+++ projetos/instagram-additional/script.js
@@ -1,23 +1,44 @@
-const contain = document.querySelector('#contain')
+// const contain = document.querySelector('#contain')
+// contain.style.display = 'none';
+// function fall() {
+//     if(contain.style.display === 'none'){
+//         contain.style.display = 'block'
+//     }else{
+//         contain.style.display = 'none'
+//     }
+// };
+
+
+// document.body.style.backgroundColor = 'black';
+// function tema() {
+//     if(document.body.style.backgroundColor === 'black'){
+//         document.body.style.backgroundColor = 'white';
+//         document.querySelectorAll('a').style.color = 'black';
+        
+
+//     }else{
+//         document.body.style.backgroundColor = 'black';
+//         document.body.style.color = 'white';
+//     }
+// };
+
+
+const contain = document.querySelector('#contain');
 contain.style.display = 'none';
-function fall() {
+
+function fall(){
     if(contain.style.display === 'none'){
-        contain.style.display = 'block'
+        contain.style.display = 'block';
     }else{
-        contain.style.display = 'none'
+        contain.style.display = 'none';
     }
 };
 
-
-document.body.style.backgroundColor = 'black';
-function tema() {
-    if(document.body.style.backgroundColor === 'black'){
-        document.body.style.backgroundColor = 'white';
-        document.querySelectorAll('a').style.color = 'black';
-        
-
+const body = document.body;
+function tema(){
+    if(body.classList.contains("white")){
+        body.classList.remove("white");
     }else{
-        document.body.style.backgroundColor = 'black';
-        document.body.style.color = 'white';
+        body.classList.add("white");
     }
 };
