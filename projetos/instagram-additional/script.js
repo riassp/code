d4c273277641ff7a6f398db9a26beec973dadbba
riassp <senpaisla374@gmail.com>
2024-04-15 15:07:49 -0300

    changes

--- projetos/instagram-additional/script.js
+++ projetos/instagram-additional/script.js
@@ -1,31 +1,5 @@
-// const contain = document.querySelector('#contain')
-// contain.style.display = 'none';
-// function fall() {
-//     if(contain.style.display === 'none'){
-//         contain.style.display = 'block'
-//     }else{
-//         contain.style.display = 'none'
-//     }
-// };
-
-
-// document.body.style.backgroundColor = 'black';
-// function tema() {
-//     if(document.body.style.backgroundColor === 'black'){
-//         document.body.style.backgroundColor = 'white';
-//         document.querySelectorAll('a').style.color = 'black';
-        
-
-//     }else{
-//         document.body.style.backgroundColor = 'black';
-//         document.body.style.color = 'white';
-//     }
-// };
-
-
 const contain = document.querySelector('#contain');
 contain.style.display = 'none';
-
 function fall(){
     if(contain.style.display === 'none'){
         contain.style.display = 'block';
@@ -34,11 +8,13 @@ function fall(){
     }
 };
 
-const body = document.body;
-function tema(){
-    if(body.classList.contains("white")){
-        body.classList.remove("white");
-    }else{
-        body.classList.add("white");
+
+function tema() {
+    const body = document.body;
+    body.style.backgroundColor = 'black';
+    if(body.style.backgroundColor = 'black'){
+        body.style.backgroundColor = 'white';
+    } else {
+        body.style.backgroundColor = 'black';
     }
 };
