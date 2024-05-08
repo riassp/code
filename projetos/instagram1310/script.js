bfd5442ad57a357efb2cdab41eb86172267d57e8
riassp <senpaisla374@gmail.com>
2024-05-08 20:09:21 -0300

    finished

--- projetos/instagram1310/script.js
+++ projetos/instagram1310/script.js
@@ -9,3 +9,11 @@ function fall(){
         contain.style.display = 'none';
     }
 };
+
+function tema(){
+    if(document.body.classList.contains("dark")){
+        document.body.classList.remove("dark");
+    }else{
+        document.body.classList.add("dark");
+    }
+};
