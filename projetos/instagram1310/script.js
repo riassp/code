30c74ab15fc55c1ca08d057d160ee7cc2ed2331c
riassp <senpaisla374@gmail.com>
2024-05-08 17:45:09 -0300

    90% complete.

---
+++ projetos/instagram1310/script.js
@@ -0,0 +1,11 @@
+const contain = document.querySelector('#contain');
+
+contain.style.display = 'none';
+
+function fall(){
+    if(contain.style.display === 'none'){
+        contain.style.display = 'block';
+    } else{
+        contain.style.display = 'none';
+    }
+};
