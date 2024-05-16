ad9fc8b63af521c985c19f3d56b1f20d93b015a1
riassp <senpaisla374@gmail.com>
2024-05-16 14:54:26 -0300

    math is cool, i think

---
+++ javascript/jsMath/ex.js
@@ -0,0 +1,46 @@
+// Exercício com base na IA
+
+// Arredondamento.
+
+/*
+function decimal(inter) {
+    return Math.round(inter);
+};
+
+const arre = decimal(9.3);
+console.log(arre);
+*/
+
+// random number.
+
+/*const teste = Math.random() * (10 - 1) + 1;
+console.log(Math.floor(teste));*/
+
+//Valor Absoluto: Escreva uma função que recebe um número como argumento e retorna o valor absoluto desse número usando Math.abs().
+
+/*const abs = Math.abs(-5);
+console.log(abs);*/
+
+// Max e min.
+
+/*const teste = Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+const teste2 = Math.min(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+console.log(teste);
+console.log(teste2);*/
+
+// Valor de PI.
+
+/*const teste = Math.PI();
+console.log(teste);*/
+
+//potência e raiz quadrada.
+
+/*function power(num1, num2) {
+    return Math.pow(num1, num2);
+};
+console.log(power(2,4)); // retorna 16.
+
+function Sroot(num){
+    return Math.sqrt(num, 2);
+};
+console.log(Sroot(9)); // retorna 3.*/
